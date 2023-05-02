4321ff81156795282ed6a3700cc95e03aa8eb7a2
abhishek <abhishek.bhattarai61067@gmail.com>
2023-05-02 17:40:57 +0545

    completed

--- src/App.vue
+++ src/App.vue
@@ -25,28 +25,29 @@
   // watching weights 
   watch(weights, newWeights => {
     const ws = [...newWeights]
+if(weightChart.value){
+  weightChart.value.data.labels =  ws.sort((a,b)=> a.date - b.date).map(w=> new Date(w.date).toLocaleDateString()).slice(-7);
+  weightChart.value.data.datasets[0].data = ws.sort((a,b)=> a.date - b.date).map(w=> w.weight).slice(-7);
+  weightChart.value.update();
+  return; 
+}
+
     nextTick(()=>{
       // console.log(weightChartEl);
         // for line chart in chartjs
 weightChart.value  = new Chart(weightChartEl.value, {
-   type: "line",
-  data: {
-    labels: ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"],
-    datasets: [
+    type: "line",
+    data: {
+    labels: ws.sort((a,b)=> a.date - b.date).map(w=> new Date(w.date).toLocaleDateString()),
+     datasets: [
       {
-        label: "Number of Moons",
-        data: [0, 0, 1, 2, 79, 82, 27, 14],
+        label: "Weight",
+        data: ws.sort((a,b)=> a.date - b.date).map(w=> w.weight) ,
         backgroundColor: "rgba(54,73,93,.5)",
         borderColor: "#36495d",
         borderWidth: 3
       },
-      {
-        label: "Planetary Mass (relative to the Sun x 10^-6)",
-        data: [0.166, 2.081, 3.003, 0.323, 954.792, 285.886, 43.662, 51.514],
-        backgroundColor: "rgba(71, 183,132,.5)",
-        borderColor: "#47b784",
-        borderWidth: 3
-      }
+      
     ]
   },
   options: {
@@ -95,7 +96,7 @@ weightChart.value  = new Chart(weightChartEl.value, {
 <h2>last 7 days</h2>
 <!-- canvas -->
 <div class="canvas-box">
-  <canvas ref="weightChartEl" id="chart"></canvas>
+  <canvas ref="weightChartEl"></canvas>
 </div>
 
 <!-- weight history -->
