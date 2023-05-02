fc97667139341f2bf17194a259a946e353665efd
abhishek <abhishek.bhattarai61067@gmail.com>
2023-05-02 15:11:20 +0545

    setup

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+<h1>test</h1>
+  </div>
+</template>
+
+<script>
+  import {shallowRef, ref, computed, watch, nextTick} from "vue";
+  import { Chart } from "chart.js/auto";
+   const weights = ref([]);
+   const weightChartEl = ref(null);
+   const weightChart= shallowRef(null);
+   const weightInput= ref(60.0);
+   const currentWeight= computed(()=>{
+      return weights.value.sort((a,b)=> b.date - a.date)[0] || {weight:0}
+   });
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
