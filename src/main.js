a7eb97120e692abeebf8b245acc5f72214b88b2d
abhishek <abhishek.bhattarai61067@gmail.com>
2023-05-08 19:04:40 +0545

    included bootstrap and font awesome

--- src/main.js
+++ src/main.js
@@ -1,5 +1,24 @@
 import { createApp } from 'vue'
+import '@fortawesome/fontawesome-free'
+
+import PortalVue from 'portal-vue'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import '@fortawesome/fontawesome-free/js/all.min.js'
+
+
+
+
+
+
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(PortalVue)
+
+app.mount('#app')
+
