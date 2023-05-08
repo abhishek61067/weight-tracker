7797db0d3044cadc2ae48ab033e835a6a7e2350b
abhishek <abhishek.bhattarai61067@gmail.com>
2023-05-08 19:14:11 +0545

    test case

--- src/main.js
+++ src/main.js
@@ -8,12 +8,6 @@ import 'bootstrap/dist/js/bootstrap.min.js'
 
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import '@fortawesome/fontawesome-free/js/all.min.js'
-
-
-
-
-
-
 import './style.css'
 import App from './App.vue'
 
